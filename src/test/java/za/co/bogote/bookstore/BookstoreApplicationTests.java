89dc1bdd6780c7ffbf17613bb60c0c4831ca37c3
RossK <kelobang.opatile@gmail.com>
2020-07-01 14:01:41 +0200

    Initial commit

---
+++ src/test/java/za/co/bogote/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.bogote.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
