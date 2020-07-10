bb725a4d128337e546d5824886c2ed59f91410ea
RossK <kelobang.opatile@gmail.com>
2020-07-10 23:24:50 +0200

    Utils created
    Signed-off-by: RossK <kelobang.opatile@gmail.com>

---
+++ src/main/java/za/co/bogote/bookstore/utility/SecurityUtility.java
@@ -0,0 +1,8 @@
+package za.co.bogote.bookstore.utility;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityUtility {
+    private static final String SALT = "salt";
+}
