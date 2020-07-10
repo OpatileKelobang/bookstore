9b06ed23201f74478be06958567677d02e4e36dc
RossK <kelobang.opatile@gmail.com>
2020-07-10 23:34:26 +0200

    Utils created
    Signed-off-by: RossK <kelobang.opatile@gmail.com>

--- src/main/java/za/co/bogote/bookstore/config/SecurityConfig.java
+++ src/main/java/za/co/bogote/bookstore/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import za.co.bogote.bookstore.service.impl.UserSecurityService;
+import za.co.bogote.bookstore.utility.SecurityUtility;
 
 @Configuration
 @EnableWebSecurity
