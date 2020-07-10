8ce350462c8dcba87889d0e6eb6048782cb0969a
RossK <kelobang.opatile@gmail.com>
2020-07-10 06:57:11 +0200

    SQL Connection Created
    Signed-off-by: RossK <kelobang.opatile@gmail.com>

--- src/main/java/za/co/bogote/bookstore/domain/User.java
+++ src/main/java/za/co/bogote/bookstore/domain/User.java
@@ -1,13 +1,85 @@
 package za.co.bogote.bookstore.domain;
 
+
+import javax.persistence.*;
+
+@Entity
 public class User {
-    private Long is;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
     private String username;
     private String password;
     private String firstName;
     private String lastName;
 
+    @Column(name = "email", nullable = false, updatable = false)
     private String email;
     private String phone;
     private boolean enabled = true;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 }
