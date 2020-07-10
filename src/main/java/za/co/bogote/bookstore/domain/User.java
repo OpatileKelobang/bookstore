7078d38e0db939ae53c3c8afdc2be7147d9f8b9a
RossK <kelobang.opatile@gmail.com>
2020-07-10 06:07:27 +0200

    User class created
    Signed-off-by: RossK <kelobang.opatile@gmail.com>

---
+++ src/main/java/za/co/bogote/bookstore/domain/User.java
@@ -0,0 +1,13 @@
+package za.co.bogote.bookstore.domain;
+
+public class User {
+    private Long is;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    private String email;
+    private String phone;
+    private boolean enabled = true;
+}
