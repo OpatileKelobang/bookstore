c260b2e32cf49c83e0042b23e81c758ab6e47b9d
RossK <kelobang.opatile@gmail.com>
2020-07-13 08:55:50 +0200

    Home controller
    Signed-off-by: RossK <kelobang.opatile@gmail.com>

--- src/main/java/za/co/bogote/bookstore/controller/HomeController.java
+++ src/main/java/za/co/bogote/bookstore/controller/HomeController.java
@@ -1,12 +1,18 @@
 package za.co.bogote.bookstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import za.co.bogote.bookstore.domain.User;
 import za.co.bogote.bookstore.domain.security.PasswordResetToken;
 import za.co.bogote.bookstore.service.UserService;
+import za.co.bogote.bookstore.service.impl.UserSecurityService;
 
 import java.util.Locale;
 
@@ -15,6 +21,9 @@ public class HomeController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private UserSecurityService userSecurityService;
 
     @RequestMapping("/")
     public String index(){
@@ -40,6 +49,25 @@ public class HomeController {
             Model model) {
 
         PasswordResetToken passwordResetToken = userService.getPasswordResetToken(token);
+
+        if (passwordResetToken == null) {
+            String message = "Invalid Token.";
+            model.addAttribute("message", message);
+            return "redirect:/badRequest";
+        }
+
+        User user = passwordResetToken.getUser();
+        String username = user.getUsername();
+
+        UserDetails userDetails = userSecurityService.loadUserByUsername(username);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                userDetails.getPassword(),
+                userDetails.getAuthorities()
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
         model.addAttribute("classActiveNewUser", true);
         return "myAccount";
